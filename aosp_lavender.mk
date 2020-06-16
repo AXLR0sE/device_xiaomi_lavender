65923688be4ee371dd5e4027a1422f086f4c04cc
Martin <recordyletsplays@gmail.com>
2020-06-16 16:26:34 +0200

    lavender: include ARCore
    fixup

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -32,6 +32,7 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Build Fingerprint
